c94dc7d0ad69a090b2851df68f5c91e35748bba2
Daniel Lindh <daniel@cybercow.se>
2019-06-29 20:02:53 +0200

    Initial commit

---
+++ src/main/kotlin/server.kt
@@ -0,0 +1,33 @@
+package se.cybercow.hellovertx
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+import java.util.concurrent.CountDownLatch
+
+class HelloVertxServer : AbstractVerticle() {
+
+    @Throws(Exception::class)
+    override fun start() {
+        val router = Router.router(vertx)
+
+        router.route().handler { routingContext ->
+            routingContext
+                .response()
+                .putHeader("content-type", "text/html")
+                .end("Hello World!")
+        }
+
+        vertx.createHttpServer().requestHandler(router).listen(8082)
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val vertx = Vertx.vertx()
+            vertx.deployVerticle(HelloVertxServer())
+
+            CountDownLatch(1).await()
+        }
+    }
+}
