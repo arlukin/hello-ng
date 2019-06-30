0001657e8ce4876882020665f0a2d497461fe1c5
Daniel Lindh <daniel@cybercow.se>
2019-06-30 15:08:11 +0200

    gitlab

---
+++ src/test/kotlin/HelloVertxServerTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+
+internal class HelloVertxServerTest {
+    @Test
+    fun getRootSucceed() {
+        assertEquals("aa", "aa")
+    }
+}
