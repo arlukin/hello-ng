e1ea116c25428e6dcadda991e8e216e67d895e39
Daniel Lindh <daniel@cybercow.se>
2019-07-16 13:51:11 +0200

    Now using ingress.

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -4,13 +4,6 @@ import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
 import java.util.concurrent.CountDownLatch
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import java.io.File
-import jdk.nashorn.internal.runtime.ScriptingFunctions.readLine
-import java.io.InputStreamReader
-import java.io.BufferedReader
-import java.io.IOException
 import java.util.*
 
 
